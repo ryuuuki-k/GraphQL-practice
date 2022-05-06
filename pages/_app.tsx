d15fb4946c8987ba4f6ffa507a924795d39f7d60
ryuki.k <ryuki.k0120@gmail.com>
2022-05-06 11:51:09 +0900

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
